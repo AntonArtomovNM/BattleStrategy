073082f47f6bfc7b775324cfdc21a7e7220c3cff
Artomov Anton <antonioartiomov@gmail.com>
2021-08-02 19:36:12 +0300

    Add more unit tests

--- UnitTests/GeneralTests.cs
+++ UnitTests/GeneralTests.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-using System.Linq;
+using System;
 using BattleStrategy;
 using BattleStrategy.Warriors;
 using BattleStrategy.Battles;
@@ -246,8 +246,208 @@ namespace BattleStrategy.UnitTests
         }
     }
 
-    //[TestFixture]
-    class BattleTests
+    [TestFixture]
+    class ArmyTests
+    {
+        ArmyUnitTrainer trainer;
+        WarriorBuilder warriorBuilder;
+        KnightBuilder knightBuilder;
+        DefenderBuilder defenderBuilder;
+        VampireBuilder vampireBuilder;
+        LancerBuilder lancerBuilder;
+        HealerBuilder healerBuilder;
+        Rank[] ranks;
+
+        [SetUp]
+        public void SetUp()
+        {
+            trainer = new ArmyUnitTrainer();
+            warriorBuilder = new WarriorBuilder();
+            knightBuilder = new KnightBuilder();
+            defenderBuilder = new DefenderBuilder();
+            vampireBuilder = new VampireBuilder();
+            lancerBuilder = new LancerBuilder();
+            healerBuilder = new HealerBuilder();
+            ranks = new Rank[]
+            {
+                new Rank(new Warrior[]{ trainer.Create(lancerBuilder), trainer.Create(healerBuilder)}),
+                new Rank(new Warrior[]{ trainer.Create(vampireBuilder) }),
+                new Rank(new Warrior[]{ trainer.Create(knightBuilder), trainer.Create(warriorBuilder), trainer.Create(warriorBuilder) }),
+                new Rank(new Warrior[]{ trainer.Create(defenderBuilder), trainer.Create(healerBuilder) }),
+                new Rank(new Warrior[]{ trainer.Create(defenderBuilder) })
+            };
+        }
+
+        [Test]
+        public void CreatingArmy_NoParams_ReturnsTrue()
+        {
+            //Arrange
+            Army army;
+            int expectedCount;
+            int actualCount;
+
+            //Act
+            army = new Army();
+            expectedCount = 0;
+            actualCount = army.Count;
+
+            //Assert
+            Assert.IsNotNull(army);
+            Assert.IsFalse(army.IsAlive);
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [Test]
+        public void CreatingArmy_WithIEnumerable_ReturnsTrue()
+        {
+            //Arrange
+            Army army;
+            int expectedCount;
+            int actualCount;
+
+            //Act
+            army = new Army(ranks);
+            expectedCount = ranks.Length;
+            actualCount = army.Count;
+
+            //Assert
+            Assert.IsNotNull(army);
+            Assert.IsTrue(army.IsAlive);
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [Test]
+        public void AddingRank_EmptyArmyOneRank_ReturnsTrue()
+        {
+            //Arrange
+            Army army;
+            int expectedCount;
+            int actualCount;
+
+            //Act
+            army = new Army();
+            army.AddRank(ranks[0]);
+            expectedCount = 1;
+            actualCount = army.Count;
+
+            //Assert
+            Assert.AreEqual(army[0], ranks[0]);
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [Test]
+        public void AddingRank_EmptyArmyArrayOfRanks_ReturnsTrue()
+        {
+            //Arrange
+            Army army;
+
+            //Act
+            army = new Army();
+            army.AddRank(ranks);
+
+            //Assert
+            CollectionAssert.AreEqual(ranks, army);
+        }
+
+        [Test]
+        public void AddingRank_EmptyArmyArrayOfUnits_ReturnsTrue()
+        {
+            //Arrange
+            Army army;
+            Warrior[] warriors = new Warrior[ranks[0].Count];
+            for(int i = 0; i < warriors.Length; i++)
+            {
+                warriors[i] = ranks[0][i];
+            }
+
+            //Act
+            army = new Army();
+            army.AddRank(warriors);
+
+            //Assert
+            CollectionAssert.AreEqual(warriors, army[0]);
+        }
+
+        [Test]
+        public void AddingRank_NotEmptyArmyArrayOfRanks_ReturnsTrue()
+        {
+            //Arrange
+            Army army;
+            int expectedCount;
+            int actualCount;
+
+            //Act
+            army = new Army();
+            army.AddRank(new Rank(new Warrior[] { trainer.Create(knightBuilder), trainer.Create(warriorBuilder) }));
+            int startCount = army.Count;
+            army.AddRank(ranks);
+            expectedCount = startCount + ranks.Length;
+            actualCount = army.Count;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [Test]
+        public void AddingToRank_NotEmptyArmyOneRank_ReturnsTrue()
+        {
+            //Arrange
+            Army army;
+            int expectedCount;
+            int actualCount;
+
+            //Act
+            army = new Army(ranks);
+            expectedCount = ranks[0].Count + 2;
+            army.AddToRank(new Rank(new Warrior[] { trainer.Create(knightBuilder), trainer.Create(warriorBuilder) }), 0);
+            actualCount = army[0].Count;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [Test]
+        public void AddingToRank_NotEmptyArmyArrayOfUnits_ReturnsTrue()
+        {
+            //Arrange
+            Army army;
+            int expectedCount;
+            int actualCount;
+
+            //Act
+            army = new Army(ranks);
+            expectedCount = ranks[0].Count + ranks[1].Count;
+            army.AddToRank(ranks[1], 0);
+            actualCount = army[0].Count;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [Test]
+        public void AddingToRank_NotEmptyArmyArrayOfUnitsWhrongIndex_ThrowsException()
+        {
+            //Arrange
+            Army army;
+            int expectedCount;
+            int actualCount;
+
+            //Act
+            void TryAddingToRank()
+            {
+                army = new Army(ranks);
+                army.AddToRank(ranks[1], ranks.Length);
+                expectedCount = ranks[0].Count + ranks[1].Count;
+                actualCount = army[0].Count;
+            }
+
+            //Assert
+            Assert.Catch(typeof(ArgumentOutOfRangeException), TryAddingToRank);
+        }
+    }
+
+    [TestFixture]
+    class BattleTests 
     {
         Battle battle;
         Army army1;
@@ -420,6 +620,34 @@ namespace BattleStrategy.UnitTests
 
             //Assert
             Assert.IsFalse(result);
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [Test]
+        public void FightRankvsRank_LancerAndHealerVsWarriorAndHealer_ReturnsTrue()
+        {
+            //Arrange
+            bool result;
+            int expectedCount;
+            int actualCount;
+            Rank rank1 = new Rank(new Warrior[]
+            {
+                trainer.Create(lancerBuilder),
+                trainer.Create(healerBuilder)
+            });
+            Rank rank2 = new Rank(new Warrior[]
+            {
+                trainer.Create(warriorBuilder),
+                trainer.Create(healerBuilder)
+            });
+
+            //Act
+            result = battle.Fight(rank1, rank2);
+            expectedCount = 2;
+            actualCount = rank1.Count;
+
+            //Assert
+            Assert.IsTrue(result);
             Assert.AreEqual(expectedCount, actualCount);
         }
     }
